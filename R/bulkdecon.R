7c323e2bedef691171b2272487513c481d7205ab
Shani Amarasinghe <shani.amarasinghe@monash.edu>
2025-11-25 11:35:20 +1100

    update main

--- R/bulkdecon.R
+++ R/bulkdecon.R
@@ -1,5 +1,5 @@
-bulkdecon <- function(norm, 
-                         bg, 
+bulkdecon <- function(norm,
+                         bg,
                          X = NULL,
                          raw = NULL,
                          wts = NULL,
@@ -9,9 +9,9 @@ bulkdecon <- function(norm,
                          cell_counts = NULL,
                          cellmerges = NULL,
                          maxit = 1000){
-    
+
     #### preliminaries ---------------------------------
-    
+
     # check formatting:
     if (!is.matrix(norm)) {
         stop("norm should be a matrix")
@@ -28,24 +28,24 @@ bulkdecon <- function(norm,
     if ((length(cell_counts) > 0) & (!is.numeric(cell_counts))) {
         stop("cell_counts must be numeric")
     }
-    
-    
+
+
     if (length(bg) == 1) {
         bg <- matrix(bg, nrow(norm), ncol(norm),
                      dimnames = list(rownames(norm), colnames(norm))
         )
     }
-    
+
     # If a matrix other than safeTME is input, rescale training matrix to avoid bad convergence properties:
     if (length(X) > 0) {
         # rescale matrix so its 99th percentile is near that of safeTME (which has a 99th percentile = 2.3)
         X <- X * 2 / quantile(X, 0.99)
     }
-    
+
     # prep training matrix:
     if (length(X) == 0) {
         utils::data("safeTME", envir = environment())
-        X <- SpatialDecon::safeTME
+        X <- BulkDecon::safeTME
     }
     sharedgenes <- intersect(rownames(norm), rownames(X))
     if (length(sharedgenes) == 0) {
@@ -58,18 +58,18 @@ bulkdecon <- function(norm,
                 to support accurate deconvolution."
         ))
     }
-    
+
     # calculate weights based on expected SD of counts
     # wts = replace(norm, TRUE, 1)
     if (length(raw) > 0) {
         weight.by.TIL.resid.sd <-
-            length(intersect(colnames(X), colnames(SpatialDecon::safeTME))) > 10
+            length(intersect(colnames(X), colnames(BulkDecon::safeTME))) > 10
         wts <- deriveWeights(norm,
                              raw = raw, error.model = "dsp",
                              weight.by.TIL.resid.sd = weight.by.TIL.resid.sd
         )
     }
-    
+
     #### if pure tumor samples are specified, get tumor expression profile --------
     if (sum(is_pure_tumor) > 0) {
 
@@ -84,8 +84,8 @@ bulkdecon <- function(norm,
 
         sharedgenes <- intersect(rownames(norm), rownames(X))
     }
-    
-    
+
+
     #### Run decon  -----------------------------------
     res <- algorithm2(
         Y = norm[sharedgenes, ],
@@ -96,10 +96,10 @@ bulkdecon <- function(norm,
         resid_thresh = resid_thresh,
         lower_thresh = lower_thresh
     )
-    
-    
+
+
     #### combine closely-related cell types ------------------------------------
-    
+
     if (length(cellmerges) > 0) {
         tempconv <- convertCellTypes(
             beta = res$beta,
@@ -115,8 +115,8 @@ bulkdecon <- function(norm,
         res$beta <- tempconv$beta
         res$sigma <- tempconv$sigma
     }
-    
-    
+
+
     #### compute p-values -------------------------------------------
     tempbeta <- res$beta
     tempse <- tempp <- tempt <- tempbeta * NA
@@ -128,11 +128,11 @@ bulkdecon <- function(norm,
     res$p <- tempp
     res$t <- tempt
     res$se <- tempse
-    
-    
+
+
     #### rescale abundance estimates --------------------------------
     # (to proportions of total, proportions of immune, cell counts)
-    
+
     # proportions:
     res$prop_of_all <- sweep(res$beta, 2, colSums(res$beta), "/")
     nontumorcellnames <- rownames(res$beta)[!grepl("tumor", rownames(res$beta))]
@@ -140,7 +140,7 @@ bulkdecon <- function(norm,
         res$beta[nontumorcellnames, ], 2,
         colSums(res$beta[nontumorcellnames, ]), "/"
     )
-    
+
     # on scale of cell counts:
     if (length(cell_counts) > 0) {
         res$cell.counts <- convertCellScoresToCounts(
@@ -156,7 +156,7 @@ bulkdecon <- function(norm,
             )
         }
     }
-    
+
     # add other pertinent info to res:
     res$X <- X[rownames(res$resids), ]
     return(res)
